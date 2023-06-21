dbabbe048febd31bad7d2c6cf579c1d544052276
kellerlivia <rm94275@fiap.com.br>
2023-06-21 19:31:50 -0300

    feat: extensions e model

---
+++ music/music/Extension/NSTextAttachment + Extension.swift
@@ -0,0 +1,17 @@
+//
+//  NSTextAttachment + Extension.swift
+//  music
+//
+//  Created by Livia Keller on 18/06/23.
+//
+
+import UIKit
+
+extension NSTextAttachment {
+    func setImageHeight(height: CGFloat) {
+        guard let image = image else { return }
+        let ratio = image.size.width / image.size.height
+        
+        bounds = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: ratio * height, height: height)
+    }
+}
---
+++ music/music/Extension/UIView + Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIView + Extension.swift
+//  music
+//
+//  Created by Livia Keller on 18/06/23.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+    }
+}
